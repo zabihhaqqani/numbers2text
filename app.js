cdf05ecb933667da8d94d5606e7e8f56668eb23e
Zabih-Haqqani <mdzabihhaqqani@gmail.com>
2022-01-01 21:55:54 +0530

    refactor

--- app.js
+++ app.js
@@ -1,35 +1,31 @@
 //reference buttons
 let translateButton = document.querySelector("#btnTranslate");
-let input = document.querySelector("#inputText")
-let output = document.querySelector("#outputText")
+let input = document.querySelector("#inputText");
+let output = document.querySelector("#outputText");
 
-btnTranslate.addEventListener("click", function clickEventHandler(){
-    console.log("clicked")
-   
-});
-
-// api 
+// api
 let serverURL = "https://api.funtranslations.com/translate/numbers.json";
 
 function getTranslationURL(text) {
-    return serverURL + "?" + "text=" + text ;
+  return serverURL + "?" + "text=" + text;
 }
 
+//error message
 function errorHandler(error) {
-    console.log("An error occurred", error);
-    alert("Server down, please try again later");
+  console.log("An error occurred", error);
+  alert("Server down, please try again after some time!!!");
 }
 
-function clickEventHandler() {  
-    let inputText = input.value; //taking input
-    output.innerText = "Translating....."
-    fetch(getTranslationURL(inputText))
-    .then(response => response.json())
-    .then(json => {
-        let translatedText = json.contents.translated;
-        output.innerText = translatedText; // giving output
+function clickEventHandler() {
+  let inputText = input.value; //taking input
+  output.innerText = "Translating.....";
+  fetch(getTranslationURL(inputText))
+    .then((response) => response.json())
+    .then((json) => {
+      let translatedText = json.contents.translated;
+      output.innerText = translatedText; // giving output
     })
-    .catch (errorHandler)
-};
+    .catch(errorHandler);
+}
 
-btnTranslate.addEventListener("click",clickEventHandler)
+btnTranslate.addEventListener("click", clickEventHandler);
